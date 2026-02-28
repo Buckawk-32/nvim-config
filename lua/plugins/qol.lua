3af32eba5a5b7260751f4e499365639196429b4e
buckawk32 <buckawk32.bit@gmail.com>
2026-03-01 03:02:56 +0800

    Added Better Comments to QoL plugins

--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -1,5 +1,43 @@
 return {
     {
+        "Djancyp/better-comments.nvim",
+        lazy = false,
+        config = function ()
+            require("better-comment").Setup({
+                tags = {
+                    {
+                        name = "TODO",
+                        fg = "white",
+                        bg = "#0a7aca",
+                        bold = true,
+                        virtual_text = "",
+                    },
+                    {
+                        name = "FIX",
+                        fg = "white",
+                        bg = "#f44747",
+                        bold = true,
+                        virtual_text = "",
+                    },
+                    {
+                        name = "WARNING",
+                        fg = "#FFA500",
+                        bg = "",
+                        bold = false,
+                        virtual_text = "",
+                    },
+                    {
+                        name = "!",
+                        fg = "#f44747",
+                        bg = "",
+                        bold = true,
+                        virtual_text = "",
+                    }
+                }
+            })
+        end,
+    },
+    {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
         config = true
